481db40358e545df8c02065346e33d3b489bbcc4
caffranksereia <circuncisaofabio@icloud.com>
2023-11-08 14:56:05 -0300

    route teste

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,11 @@
 const routes = require('express').Routes();
-
+// const {User} = require('./app/models')
 //define as routas
 
+
+// User.create({
+//     name:"Fabio Eduardo",
+//     email:"fabioeduardo@hotmail.com",
+//     password_hash: "12346547788"
+// })
 module.exports = routes;
