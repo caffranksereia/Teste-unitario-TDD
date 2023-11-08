78bef32630337d928e27db9a281c7405f89b1a00
caffranksereia <circuncisaofabio@icloud.com>
2023-11-08 14:55:05 -0300

    teste jest

---
+++ __tests__/integration/session.test.js
@@ -0,0 +1,11 @@
+describe('Authentication',()=>{
+    it('sshould sum two numbers',
+    () =>{
+        const x =2;
+        const y=4;
+
+        const sum = x+y;
+
+        expect(sum).toBe(6);
+    });
+});
