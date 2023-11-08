799111ca6d74af886dd24f392edd614cb7a63928
caffranksereia <circuncisaofabio@icloud.com>
2023-11-08 15:31:43 -0300

    config env e dotenv

--- src/config/config.js
+++ src/config/config.js
@@ -1,14 +1,20 @@
+require("dotenv").config({
+  path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
+});
+
 module.exports ={
-  host:,
-  username:,
-  password: ,
-  database: ,
-  dialect: ,
+  host:process.env.db_host,
+  username:process.env.db_username,
+  password: process.env.db_password,
+  database:process.env.db_database ,
+  dialect: process.env.db_dialect || "postgres",
+  storage: './__tests__/database.sqlite',
   operatorsAlioases: false,
   logging: false,
   define:{
     timestamps: true,
     underscored: true,
+    underscoredAll: true
   }
   
 };
