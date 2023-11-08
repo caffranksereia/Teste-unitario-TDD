d777c6158a254682f69a92c57c4d544da654efea
caffranksereia <circuncisaofabio@icloud.com>
2023-11-08 09:26:10 -0300

    COnfig do squelize: configuracao do sequelize

---
+++ src/config/config.js
@@ -0,0 +1,14 @@
+module.exports ={
+  host:'127.0.0.1',
+  username: "root",
+  password: "docker",
+  database: "databaseauth",
+  dialect: "postgres",
+  operatorsAlioases: false,
+  logging: false,
+  define:{
+    timestamps: true,
+    underscored: true,
+  }
+  
+};
