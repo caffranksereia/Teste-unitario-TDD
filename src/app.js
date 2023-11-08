d40eedbbf8023ac360e6dfcafafbf5327f8aaf12
caffranksereia <circuncisaofabio@icloud.com>
2023-11-08 09:04:37 -0300

    logica de criacao: se paro essa aplicacao para quando executar os teste quero que nao aloca a porta 3000 , roda o teste dentro da aplicacao

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+//logica de criacao do servidor
+class AppController{
+    constructor(){
+        this.express = espress();
+
+        this.middlewares();
+        this.routes();
+    }
+    middlewares(){
+        this.express.use(express.json());
+
+    }
+    routes(){
+        this.express.use(require('/rotes'));
+    }
+}
+
+module.express = new AppController().express;
